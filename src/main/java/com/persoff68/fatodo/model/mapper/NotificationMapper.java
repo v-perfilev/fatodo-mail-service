30f575528a0e9fb1a0425ae11f27e87ff9c192bd
Vladimir Perfilev <persoff68@gmail.com>
2021-11-26 01:20:29 +0100

    TDM-112 fix code smells

--- src/main/java/com/persoff68/fatodo/model/mapper/NotificationMapper.java
+++ src/main/java/com/persoff68/fatodo/model/mapper/NotificationMapper.java
@@ -1,8 +1,6 @@
 package com.persoff68.fatodo.model.mapper;
 
-import com.persoff68.fatodo.model.Activation;
 import com.persoff68.fatodo.model.Notification;
-import com.persoff68.fatodo.model.dto.ActivationDTO;
 import com.persoff68.fatodo.model.dto.NotificationDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
