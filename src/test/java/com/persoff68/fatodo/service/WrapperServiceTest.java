6070fcb625a4b934ab31cddde21bd8d770d97e3d
Vladimir Perfilev <persoff68@gmail.com>
2023-08-05 13:12:17 +0200

    TDM-206 add ES language

--- src/test/java/com/persoff68/fatodo/service/WrapperServiceTest.java
+++ src/test/java/com/persoff68/fatodo/service/WrapperServiceTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest(classes = FatodoMailServiceApplication.class)
 class WrapperServiceTest {
 
-    private final static String[] LANGUAGES = {"EN", "RU"};
+    private final static String[] LANGUAGES = {"EN", "ES", "RU"};
 
     @Autowired
     WrapperService wrapperService;
